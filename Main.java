23515d2bbe8742b2a3628a02f81d899bc1744eac
Raj Tamang <raaj08236@gmail.com>
2026-03-06 18:04:01 +0545

    Main

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+import java.util.*;
 
-class Main{
+public class Main {
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        List<String> code = new ArrayList<>();
+
+        System.out.println("Mini Python (type 'run' to execute)");
+
+        while(true) {
+
+            String line = sc.nextLine();
+
+            if(line.equals("run"))
+                break;
+
+            code.add(line);
+        }
+
+        Parser parser = new Parser();
+        Interpreter interpreter = new Interpreter();
+
+        List<Statement> program = parser.parse(code);
+
+        interpreter.run(program);
     }
 }
